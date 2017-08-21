63d5e4e798e3da274b16fd64361e76a76bdc551f
Unknown <jorgemario094@gmail.com>
2017-08-21 15:34:36 -0600

    Development 2

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main()
         printf("Hola mundo");
         printf("Hola mundo again");
         printf("Hola mundo por ultima vez");
+        printf("Hola mundo development2");
         return 0;
 }
--- hello.h
+++ hello.h
@@ -5,5 +5,6 @@ int main()
         printf("Hola mundo");
         printf("Hola mundo again huehue");
         printf("Hola mundo huehue final");
+        printf("Hola mundo development 2");
         return 0;
 }
